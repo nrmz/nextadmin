9db86e60f7c216bf79691795a7f2d9b5082d9cc8
nasser ramezanpour <na.rmpr@gmail.com>
2023-12-03 09:27:54 +0330

    rightbar dashboard done

--- app/ui/dashboard/rightbar/rightbar.jsx
+++ app/ui/dashboard/rightbar/rightbar.jsx
@@ -1,7 +1,53 @@
+import { MdPlayCircleFilled } from "react-icons/md";
 import styles from "./rightbar.module.css";
+import Image from "next/image";
 
 const Rightbar = () => {
-  return <div className={styles.container}>Rightbar</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <div className={styles.bgContainer}>
+          <Image src="/astronaut.png" alt="" fill className={styles.bg} />
+        </div>
+        <div className={styles.text}>
+          <span className={styles.notification}>Available Now</span>
+          <h3 className={styles.title}>
+            How to use the new version of admin dashboard?
+          </h3>
+          <span className={styles.subtitle}>Takes 4 minutes to learn</span>
+          <p className={styles.desc}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita
+            esse nemo voluptate fugit ea.
+          </p>
+          <button className={styles.button}>
+            <MdPlayCircleFilled /> Watch
+          </button>
+        </div>
+      </div>
+
+      <div className={styles.item}>
+        <div className={styles.bgContainer}>
+          <Image src="/astronaut.png" alt="" fill className={styles.bg} />
+        </div>
+        <div className={styles.text}>
+          <span className={styles.notification}></span>
+          <h3 className={styles.title}>
+            How to use the new version of admin dashboard?
+          </h3>
+          <span className={styles.subtitle}>Takes 4 minutes to learn</span>
+          <p className={styles.description}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita
+            esse nemo voluptate fugit ea. Soluta saepe mollitia ipsa ducimus
+            cupiditate, nostrum aut, numquam perferendis doloribus quia,
+            voluptatibus repellendus id? Natus.
+          </p>
+          <button className={styles.button}>
+            <MdPlayCircleFilled /> Watch
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Rightbar;
