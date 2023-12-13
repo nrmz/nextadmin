5bfd4ef899e6235b7f57b6efd17af965575a72e1
nasser ramezanpour <na.rmpr@gmail.com>
2023-12-13 08:21:16 +0330

    add login page

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,7 +1,16 @@
-const LoginPage = () => {
-    return (
-        <div>Login Page</div>
-    )
-}
+import styles from "@/app/ui/login/login.module.css";
 
-export default LoginPage
+const LoginPage = () => {
+  return (
+    <div className={styles.container}>
+      <form action="" className={styles.form}>
+        <h1>Login</h1>
+        <input type="text" placeholder="username" />
+        <input type="password" placeholder="password" />
+        <button>Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default LoginPage;
